7f0bbf6b2794e75646e10f5a0555a0b05c331d94
keshu <keshujangid191@gmail.com>
2024-08-04 08:31:46 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const ejs = require('ejs');
+const path = require("path")
+const app = express();
+const router = require('./routes/index')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+
+app.use('/', router)
+
+
+app.listen(3000)
