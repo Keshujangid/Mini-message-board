8da748ee81dded5d530b496b53b5436ca6dd4dcc
keshu <keshujangid191@gmail.com>
2024-08-21 20:00:05 +0530

    modify environment variable

--- db/pool.js
+++ db/pool.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || `postgresql://${process.env.USER_NAME}:${process.env.PASSWORD}@localhost:5432/top_users`
-
+  
 });
 
 module.exports = pool;
